daa39022096a933047bcc1d0d69a8d7c15907f51
elawbek <kdmitrii86@gmail.com>
2023-01-27 19:35:21 +0300

    init

---
+++ hardhat.config.ts
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.17",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+    ],
+  },
+};
+
+export default config;
